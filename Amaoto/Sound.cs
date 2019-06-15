42c19f922dccb19231efffc01af14ff1a622d05e
AioiLight <info@aioilight.space>
2019-06-15 10:03:27 +0900

    サウンド管理を行うクラス。

---
+++ Amaoto/Sound.cs
@@ -0,0 +1,133 @@
+﻿using DxLibDLL;
+using System;
+
+namespace Amaoto
+{
+    /// <summary>
+    /// サウンド管理を行うクラス。
+    /// </summary>
+    public class Sound : IDisposable
+    {
+        /// <summary>
+        /// サウンドを生成します。
+        /// </summary>
+        public Sound(string fileName)
+        {
+            ID = DX.LoadSoundMem(fileName);
+            if (ID != -1)
+            {
+                IsEnable = true;
+            }
+            else
+            {
+                throw new Exception();
+            }
+            FileName = fileName;
+        }
+
+        ~Sound()
+        {
+            if (IsEnable)
+            {
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (DX.DeleteSoundMem(ID) != -1)
+            {
+                IsEnable = false;
+            }
+        }
+
+        /// <summary>
+        /// サウンドを再生します。
+        /// </summary>
+        /// <param name="playFromBegin">はじめから</param>
+        public void Play(bool playFromBegin = true)
+        {
+            if (IsEnable)
+            {
+                DX.PlaySoundMem(ID, DX.DX_PLAYTYPE_BACK, playFromBegin ? 1 : 0);
+            }
+        }
+
+        /// <summary>
+        /// サウンドを停止します。
+        /// </summary>
+        public void Stop()
+        {
+            if (IsEnable)
+            {
+                DX.StopSoundMem(ID);
+            }
+        }
+
+
+        /// <summary>
+        /// 有効かどうか。
+        /// </summary>
+        public bool IsEnable { get; private set; }
+        /// <summary>
+        /// ファイル名。
+        /// </summary>
+        public string FileName { get; private set; }
+        /// <summary>
+        /// ID。
+        /// </summary>
+        public int ID { get; private set; }
+        /// <summary>
+        /// 再生中かどうか。
+        /// </summary>
+        public bool IsPlaying
+        {
+            get
+            {
+                return DX.CheckSoundMem(ID) == 1;
+            }
+        }
+        /// <summary>
+        /// パン。
+        /// </summary>
+        public int Pan
+        {
+            get
+            {
+                return _pan;
+            }
+            set
+            {
+                _pan = value;
+                DX.ChangePanSoundMem(value, ID);
+            }
+        }
+        /// <summary>
+        /// 音量。
+        /// </summary>
+        public int Volume
+        {
+            get
+            {
+                return _volume;
+            }
+            set
+            {
+                _volume = value;
+                DX.ChangeVolumeSoundMem(value, ID);
+            }
+        }
+        /// <summary>
+        /// 再生位置。
+        /// </summary>
+        public int Position
+        {
+            get
+            {
+                return DX.GetSoundCurrentPosition(ID);
+            }
+        }
+        private int _pan;
+        private int _volume;
+    }
+}
