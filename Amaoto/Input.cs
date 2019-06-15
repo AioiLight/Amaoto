357a78e59a06b46b4cc8264b59c1c87077949f2b
AioiLight <info@aioilight.space>
2019-06-15 11:05:01 +0900

    入力を管理するクラス。

---
+++ Amaoto/Input.cs
@@ -0,0 +1,59 @@
+﻿using DxLibDLL;
+
+namespace Amaoto
+{
+    /// <summary>
+    /// 入力を管理するクラス。
+    /// </summary>
+    public class Input
+    {
+        public Input()
+        {
+            Keys = new byte[256];
+            Buffer = new byte[256];
+        }
+
+        /// <summary>
+        /// キー入力の状態をチェックします。毎フレーム呼び出す必要があります。
+        /// </summary>
+        public void Update()
+        {
+            DX.GetHitKeyStateAll(Buffer);
+            for (int i = 0; i < 256; i++)
+            {
+                if (Buffer[i] == 1)
+                {
+                    if (Keys[i] == 0) Keys[i] = 1;
+                    else if (Keys[i] == 1) Keys[i] = 2;
+                }
+                else
+                {
+                    Keys[i] = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// そのキーが入力されたかどうかチェックします。
+        /// </summary>
+        /// <param name="key">キーコード。</param>
+        /// <returns>入力されたかどうか。</returns>
+        public bool IsPushedKey(int key)
+        {
+            return Keys[key] == 1;
+        }
+
+        /// <summary>
+        /// そのキーが入力されているかどうかチェックします。
+        /// </summary>
+        /// <param name="key">キーコード。</param>
+        /// <returns>入力されているかどうか。</returns>
+        public bool IsPushingKey(int key)
+        {
+            return Keys[key] > 0;
+        }
+
+        private readonly byte[] Keys;
+        private readonly byte[] Buffer;
+    }
+}
