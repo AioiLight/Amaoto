411f07b4deea5f8b790eed0f2553d30d1757ed76
AioiLight <info@aioilight.space>
2019-06-16 10:28:40 +0900

    イーズイン・イーズインアウト・イーズアウト・フェードイン

---
+++ Amaoto/Animation/EaseOut.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaoto.Animation
+{
+    /// <summary>
+    /// イーズアウトを行うクラス。
+    /// </summary>
+    public class EaseOut : Animator
+    {
+        /// <summary>
+        /// イーズアウトを初期化します。
+        /// </summary>
+        /// <param name="startPoint">始点。</param>
+        /// <param name="endPoint">終点。</param>
+        /// <param name="timeMs">イージングにかける時間。</param>
+        public EaseOut(int startPoint, int endPoint, int timeMs) : base(0, timeMs - 1, 1, false)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Sa = EndPoint - StartPoint;
+            TimeMs = timeMs;
+        }
+        /// <summary>
+        /// 座標を返します。
+        /// </summary>
+        /// <returns>double型の座標。</returns>
+        public override object GetAnimation()
+        {
+            var persent = Counter.Value / (double)TimeMs;
+            persent -= 1;
+            return (double)Sa * (persent * persent * persent + 1) + StartPoint;
+        }
+
+        private readonly int StartPoint;
+        private readonly int EndPoint;
+        private readonly int Sa;
+        private readonly int TimeMs;
+    }
+}
---
+++ Amaoto/Animation/FadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaoto.Animation
+{
+    /// <summary>
+    /// フェードインを行うクラス。
+    /// </summary>
+    public class FadeIn : Animator
+    {
+        /// <summary>
+        /// フェードインを初期化します。
+        /// </summary>
+        /// <param name="timems">フェードインに掛ける秒数(ミリ秒)</param>
+        public FadeIn(int timems) : base(0, timems - 1, 1, false)
+        {
+            TimeMs = timems;
+        }
+
+        /// <summary>
+        /// フェードインの不透明度を0～1の範囲で返します。
+        /// </summary>
+        /// <returns>不透明度。</returns>
+        public override object GetAnimation()
+        {
+            var opacity = base.Counter.Value / TimeMs;
+            return opacity;
+        }
+
+        private readonly int TimeMs;
+    }
+}
