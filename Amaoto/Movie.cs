20ce25f49a1aae50d2ca6df856fd7a003c4b9cca
AioiLight <info@aioilight.space>
2019-07-30 00:50:55 +0900

    動画再生機能。

---
+++ Amaoto/Movie.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+
+namespace Amaoto
+{
+    /// <summary>
+    /// 動画再生クラス。
+    /// </summary>
+    public class Movie : Texture
+    {
+        /// <summary>
+        /// 動画ファイルのオープンを行います。
+        /// </summary>
+        /// <param name="fileName">ファイル名。</param>
+        public Movie(string fileName)
+            : base(fileName)
+        {
+
+        }
+
+        /// <summary>
+        /// 再生を開始します。
+        /// </summary>
+        public void Play()
+        {
+            DX.PlayMovieToGraph(ID);
+        }
+
+        /// <summary>
+        /// 再生を停止します。
+        /// </summary>
+        public void Stop()
+        {
+            DX.PauseMovieToGraph(ID);
+        }
+
+        /// <summary>
+        /// 時間。
+        /// </summary>
+        public int Time
+        {
+            get
+            {
+                return DX.TellMovieToGraph(ID);
+            }
+            set
+            {
+                DX.SeekMovieToGraph(ID, value);
+            }
+        }
+
+        /// <summary>
+        /// 再生中かどうか。
+        /// </summary>
+        public bool IsPlaying
+        {
+            get
+            {
+                return DX.GetMovieStateToGraph(ID) == 1 ? true : false;
+            }
+        }
+    }
+}
