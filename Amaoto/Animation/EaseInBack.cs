612b14c243c26a5a8c9da02220728b6d88107740
AioiLight <info@aioilight.space>
2020-08-03 00:52:44 +0900

    イーズイン・アウトバックの追加

---
+++ Amaoto/Animation/EaseInBack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaoto.Animation
+{
+    /// <summary>
+    /// イーズインバックを行うクラス。
+    /// </summary>
+    public class EaseInBack : Animator
+    {
+        /// <summary>
+        /// イーズインバックを初期化します。
+        /// </summary>
+        /// <param name="startPoint">始点。</param>
+        /// <param name="endPoint">終点。</param>
+        /// <param name="timeNs">イージングにかける時間。</param>
+        public EaseInBack(int startPoint, int endPoint, int timeNs) : base(0, timeNs - 1, 1, false)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Sa = EndPoint - StartPoint;
+            TimeNs = timeNs;
+        }
+        /// <summary>
+        /// 座標を返します。
+        /// </summary>
+        /// <returns>double型の座標。</returns>
+        public override double GetAnimation()
+        {
+            var persent = Counter.Value / (double)TimeNs;
+            var c1 = 1.70158;
+            var c3 = c1 + 1;
+
+            return (c3 * persent * persent * persent) - (c1 * persent * persent);
+        }
+
+        private readonly int StartPoint;
+        private readonly int EndPoint;
+        private readonly int Sa;
+        private readonly int TimeNs;
+    }
+}
---
+++ Amaoto/Animation/EaseOutBack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amaoto.Animation
+{
+    /// <summary>
+    /// イーズアウトバックを行うクラス。
+    /// </summary>
+    public class EaseOutBack : Animator
+    {
+        /// <summary>
+        /// イーズアウトバックを初期化します。
+        /// </summary>
+        /// <param name="startPoint">始点。</param>
+        /// <param name="endPoint">終点。</param>
+        /// <param name="timeNs">イージングにかける時間。</param>
+        public EaseOutBack(int startPoint, int endPoint, int timeNs) : base(0, timeNs - 1, 1, false)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Sa = EndPoint - StartPoint;
+            TimeNs = timeNs;
+        }
+        /// <summary>
+        /// 座標を返します。
+        /// </summary>
+        /// <returns>double型の座標。</returns>
+        public override double GetAnimation()
+        {
+            var persent = Counter.Value / (double)TimeNs;
+            var c1 = 1.70158;
+            var c3 = c1 + 1;
+            return Sa * (1 + c3 * Math.Pow(persent - 1, 3) + c1 * Math.Pow(persent - 1, 2)) + StartPoint;
+        }
+
+        private readonly int StartPoint;
+        private readonly int EndPoint;
+        private readonly int Sa;
+        private readonly int TimeNs;
+    }
+}
