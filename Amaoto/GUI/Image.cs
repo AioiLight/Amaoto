c0f03464aa507af1586bbf881818e4442355be79
AioiLight <info@aioilight.space>
2020-08-09 17:48:21 +0900

    Amaoto.GUI.Image

---
+++ Amaoto/GUI/Image.cs
@@ -0,0 +1,14 @@
+﻿namespace Amaoto.GUI
+{
+    /// <summary>
+    /// ただ画像表示を行うだけの GUI 部品。
+    /// </summary>
+    public class Image : DrawPart
+    {
+        public Image(Texture texture)
+            : base(texture.TextureSize.width, texture.TextureSize.height)
+        {
+            Texture = texture;
+        }
+    }
+}
