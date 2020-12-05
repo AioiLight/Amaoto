347570e8b0ed063150eb35700f4e012683d9ec4a
AioiLight <info@aioilight.space>
2020-12-06 00:20:13 +0900

    VirtualScreenの描画をラムダ式を用いて行うように (従来のメソッドは非推奨化)

--- Amaoto/VirtualScreen.cs
+++ Amaoto/VirtualScreen.cs
@@ -22,11 +22,30 @@ namespace Amaoto
         /// <summary>
         /// 仮想スクリーンに描画する。
         /// </summary>
+        /// <param name="drawing">テクスチャを描画するラムダ式。</param>
+        /// <returns>VirtualScreen。</returns>
+        public VirtualScreen Draw(Action drawing)
+        {
+            var getDrawScreen = DX.GetDrawScreen();
+
+            DX.SetDrawScreen(Texture.ID);
+
+            drawing?.Invoke();
+
+            DX.SetDrawScreen(getDrawScreen);
+            return this;
+        }
+
+        /// <summary>
+        /// 仮想スクリーンに描画する。
+        /// ラムダ式によるDraw()を使用してください。
+        /// </summary>
         /// <param name="texture">テクスチャ。</param>
         /// <param name="x">X座標。</param>
         /// <param name="y">Y座標。</param>
         /// <param name="rectangle">描画範囲。</param>
         /// <returns>VirtualScreen。</returns>
+        [Obsolete("ラムダ式によるDraw()を使用してください")]
         public VirtualScreen Draw(Texture texture, float x, float y, Rectangle? rectangle = null)
         {
             if (texture == null)
