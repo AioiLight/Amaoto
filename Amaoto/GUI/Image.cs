ad8f00adda35f8522d7fb0c65fbb12fdb4e918bd
AioiLight <info@aioilight.space>
2021-01-02 01:03:37 +0900

    Image GUI部品の修正

--- Amaoto/GUI/Image.cs
+++ Amaoto/GUI/Image.cs
@@ -5,9 +5,19 @@
     /// </summary>
     public class Image : DrawPart
     {
-        public Image(Texture texture)
-            : base(texture.TextureSize.width, texture.TextureSize.height)
+        public Image(Texture texture, int? width = null, int? height = null)
+            : base(width.HasValue ? width.Value : texture.TextureSize.width, height.HasValue ? height.Value : texture.TextureSize.height)
         {
+            if (width.HasValue)
+            {
+                texture.ScaleX = (float)(1.0 * width.Value / texture.TextureSize.width);
+            }
+
+            if (height.HasValue)
+            {
+                texture.ScaleY = (float)(1.0 * height.Value / texture.TextureSize.height);
+            }
+
             Texture = texture;
         }
     }
