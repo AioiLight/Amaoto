62e35c052b050f4508026a42ef3c21f6285893a2
AioiLight <info@aioilight.space>
2021-01-20 02:10:21 +0900

    + Amaoto.GUI.Container

---
+++ Amaoto/GUI/Container.cs
@@ -0,0 +1,17 @@
+﻿namespace Amaoto.GUI
+{
+    /// <summary>
+    /// 決まったサイズを持つコンテナを生成する。
+    /// </summary>
+    public class Container : DrawPart
+    {
+        /// <summary>
+        /// 決まったサイズを持つコンテナを生成する。
+        /// </summary>
+        public Container(int width, int height)
+            : base(width, height)
+        {
+
+        }
+    }
+}
