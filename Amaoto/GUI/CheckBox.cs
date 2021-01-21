55bd7f26f277a741c3e2d9138ab1a1844a975ed0
AioiLight <info@aioilight.space>
2021-01-22 00:36:04 +0900

    Amaoto.GUI.CheckBox コンストラクタの引数の順番変更

--- Amaoto/GUI/CheckBox.cs
+++ Amaoto/GUI/CheckBox.cs
@@ -4,27 +4,9 @@ namespace Amaoto.GUI
 {
     public class CheckBox : DrawPart
     {
-        public CheckBox(int width, int height, ITextureReturnable boxTexture, ITextureReturnable checkTexture, ITextureReturnable description, bool check)
+        public CheckBox(ITextureReturnable boxTexture, ITextureReturnable checkTexture, ITextureReturnable description, bool check, int width, int height)
             : base(width, height)
         {
-            //var textTex = description.GetTexture();
-            //// 土台の描画。
-            //var boxTex = boxTexture.GetTexture();
-            //var boxTextureSize = boxTex.TextureSize;
-
-            //var screen = new VirtualScreen(Width, Height);
-
-            //Checked = check;
-
-            //// 文字の描画
-            //textTex.ReferencePoint = ReferencePoint.CenterLeft;
-            //screen.Draw(() => textTex.Draw(boxTextureSize.Width, height / 2));
-
-            //Texture = screen.Texture;
-            //Texture.ReferencePoint = ReferencePoint.CenterLeft;
-            //CheckTex = checkTexture.GetTexture();
-            //CheckTex.ReferencePoint = ReferencePoint.CenterLeft;
-
             VirtualScreen = new VirtualScreen(width, height);
             BoxTex = boxTexture;
             CheckTex = checkTexture;
