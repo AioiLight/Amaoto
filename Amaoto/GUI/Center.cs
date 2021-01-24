314008df449249ba87aef2eb77dbb517fa51f333
AioiLight <info@aioilight.space>
2021-01-24 18:46:47 +0900

    * 不要なusingの削除

--- Amaoto/GUI/Center.cs
+++ Amaoto/GUI/Center.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Amaoto.GUI
+﻿namespace Amaoto.GUI
 {
     /// <summary>
     /// 上下中央揃えにGUI部品を描画する。
