8e2d79722f6accce81695e8cada4f21e436fae84
AioiLight <info@aioilight.space>
2021-01-28 16:15:00 +0900

    * Amaoto.GUI.Image 画像の置換に対応

--- Amaoto/GUI/Image.cs
+++ Amaoto/GUI/Image.cs
@@ -5,20 +5,47 @@
     /// </summary>
     public class Image : DrawPart
     {
+        /// <summary>
+        /// ただ画像表示を行うだけの GUI 部品。
+        /// </summary>
+        /// <param name="texture">テクスチャ。</param>
+        /// <param name="width">(オプション)横幅。指定しない場合は画像の横幅がそのまま使用される。</param>
+        /// <param name="height">(オプション)縦幅。指定しない場合は画像の縦幅がそのまま使用される。</param>
         public Image(Texture texture, int? width = null, int? height = null)
             : base(width ?? texture.TextureSize.Width, height ?? texture.TextureSize.Height)
         {
+            SetScale(texture, width, height);
+
+            Texture = texture;
+            ImageWidth = width ?? texture.TextureSize.Width;
+            ImageHeight = height ?? texture.TextureSize.Height;
+        }
+
+        /// <summary>
+        /// 新しい画像で置換する。
+        /// GUIのサイズは初期化時のサイズがそのまま使用される。
+        /// </summary>
+        /// <param name="texture">テクスチャ。</param>
+        public void ReplaceImage(Texture texture)
+        {
+            SetScale(texture, ImageWidth, ImageHeight);
+            Texture = texture;
+        }
+
+        private void SetScale(Texture texture, int? width = null, int? height = null)
+        {
             if (width.HasValue)
             {
-                texture.ScaleX = (1.0 * width.Value / texture.TextureSize.Width);
+                texture.ScaleX = 1.0 * width.Value / texture.TextureSize.Width;
             }
 
             if (height.HasValue)
             {
-                texture.ScaleY = (1.0 * height.Value / texture.TextureSize.Height);
+                texture.ScaleY = 1.0 * height.Value / texture.TextureSize.Height;
             }
-
-            Texture = texture;
         }
+
+        private readonly int ImageWidth;
+        private readonly int ImageHeight;
     }
 }
