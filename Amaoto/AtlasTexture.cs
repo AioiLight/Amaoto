9de0b9df53788b8a45e0b36188d419e0fe2283e0
AioiLight <info@aioilight.space>
2021-02-18 03:10:30 +0900

    アトラステクスチャの実装

---
+++ Amaoto/AtlasTexture.cs
@@ -0,0 +1,51 @@
+﻿using DxLibDLL;
+using System;
+using System.Drawing;
+
+namespace Amaoto
+{
+    /// <summary>
+    /// アトラスを生成する。
+    /// 単一の画像から複数のテクスチャを切り出すことで、描画の高速化を実現する。
+    /// </summary>
+    public class AtlasTexture : IDisposable
+    {
+        /// <summary>
+        /// アトラスを生成する。
+        /// </summary>
+        /// <param name="fileName">アトラスのファイル名。</param>
+        public AtlasTexture(string fileName)
+        {
+            Source = new Texture(fileName);
+        }
+
+        ~AtlasTexture()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// アトラスからテクスチャを取得する。
+        /// </summary>
+        /// <param name="rectangle">矩形。</param>
+        /// <returns>テクスチャ。</returns>
+        public Texture GetTexture(Rectangle rectangle)
+        {
+            var t = DX.DerivationGraph(rectangle.X, rectangle.Y,
+                rectangle.Width, rectangle.Height,
+                Source.ID);
+
+            return new Texture(t);
+        }
+
+        /// <summary>
+        /// アトラスを破棄する。
+        /// </summary>
+        public void Dispose()
+        {
+            Source?.Dispose();
+        }
+
+        private protected Texture Source;
+    }
+}
